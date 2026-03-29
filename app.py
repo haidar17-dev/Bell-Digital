8c67b36a3cf45d027a2455a0dcff90a74da31251
Haidar <adeelhaidar95@gmail.com>
2026-03-29 14:34:02 +0700

    .

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+import pusher
+
+pusher_client = pusher.Pusher(
+    app_id = "2134043",
+    key = "bcf0f5714b9e9a5b86d1",
+    secret = "2aa54de5497b0a0857a6",
+    cluster = "ap1",
+    ssl=True
+)
 
 @app.route("/")
 def index():
@@ -18,6 +27,10 @@ def sender():
 def receiver():
     return render_template("receiver.html")
 
-# WAJIB SEPERTI INI:
+@app.route("/push-bell", methods=["POST"])
+def push_bell():
+    pusher_client.trigger('bell-channel', 'bell-event', {'message': 'BEL DITEKAN'})
+    return jsonify({"status": "success"})
+    
 if __name__ == "__main__":
     app.run(debug=True)
